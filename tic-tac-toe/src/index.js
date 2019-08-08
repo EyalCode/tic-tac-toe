b89ad3ea9c1b3a0da236788d517a387c547a28fb
EyalCode <eyalcha@gmail.com>
2019-08-08 15:11:27 +0300

    The basic working game

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -33,7 +33,7 @@ class Board extends React.Component {
 
   handleClick(i) {
     //do ONLY IF this square is empty.
-    if(this.state.squares[i] === null) {
+    if(this.state.squares[i] === null && !calculateWinner(this.state.squares)) {
       const squares = this.state.squares.slice();
       squares[i] = (this.state.turnNumber%2 === 0) ? 'X' : 'O';
       this.setState({
@@ -45,7 +45,14 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + ((this.state.turnNumber%2 === 0) ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'The Winner is ' + winner;
+    }
+    else {
+      status = 'Next player: ' + ((this.state.turnNumber%2 === 0) ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -84,6 +91,26 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
